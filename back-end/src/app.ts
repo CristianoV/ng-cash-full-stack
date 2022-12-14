08335b66a6a640ff88c14ed4bffd037cd5855c72
CristianoV <cristianoviieira@gmail.com>
2022-12-14 00:13:57 -0300

    fix: bugs devido deploy

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -18,7 +18,14 @@ class App {
   }
 
   private config(): void {
-    this.app.use(cors());
+    app.use((req, res, next) => {
+      //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+      res.header('Access-Control-Allow-Origin', '*');
+      //Quais são os métodos que a conexão pode realizar na API
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      app.use(cors());
+      next();
+    });
     this.app.use(express.json());
     this.app.get('/', (req, res) => res.json({ status: 'ok' }));
     this.app.use('/login', Login);

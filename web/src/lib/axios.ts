6072f3dcfe5498b8d3a966ae53829ebba5763367
CristianoV <cristianoviieira@gmail.com>
2022-12-13 23:59:13 -0300

    feat: adiciona novo link para requisição

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const fetchFromApi = axios.create({
-  baseURL: 'https://ng-cash-full-stack-production.up.railway.app/',
+  baseURL: 'https://ng-cash-full-stack.up.railway.app/',
 });
